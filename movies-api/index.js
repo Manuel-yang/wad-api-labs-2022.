b99cd37e32fb6e7f734dabf454ddfd2b0534fcf4
Yimeng Yang <731270812@qq.com>
2022-11-21 12:36:24 +0000

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies/index.js';
 import router4genres from './api/movies/router4genres'
+import usersRouter from './api/users';
 import './db';
 import './seedData'
 
@@ -14,6 +15,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', router4genres)
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
