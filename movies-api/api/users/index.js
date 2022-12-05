ecbd4bf7ed0db24d56cd240997a2b4abc8e865ed
Yimeng Yang <731270812@qq.com>
2022-12-05 20:34:26 +0000

    Topic 11 - Exercises

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -19,8 +19,15 @@ router.post('/',asyncHandler( async (req, res, next) => {
     return next();
   }
   if (req.query.action === 'register') {
-    await User.create(req.body);
-    res.status(201).json({code: 201, msg: 'Successful created new user.'});
+    const reg = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/
+    if(reg.test(req.body.password)) {
+      await User.create(req.body);
+      res.status(201).json({code: 201, msg: 'Successful created new user.'});
+    }
+    else {
+      res.status(401).json({code: 401,msg: 'Password are at least 5 characters long and contain at least one number and one letter'});
+    }
+
   } else {
     const user = await User.findByUserName(req.body.username);
       if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
@@ -51,7 +58,6 @@ router.post('/',asyncHandler( async (req, res, next) => {
 });
 
 router.get('/:userName/favourites', asyncHandler( async (req, res) => {
-  console.log(req.params.userName)
   const userName = req.params.userName;
   const user = await User.findByUserName(userName).populate('favourites');
   res.status(200).json(user.favourites);
@@ -64,9 +70,16 @@ router.post('/:userName/favourites', asyncHandler(async (req, res) => {
   const userName = req.params.userName;
   const movie = await movieModel.findByMovieDBId(newFavourite);
   const user = await User.findByUserName(userName);
-  await user.favourites.push(movie._id);
-  await user.save(); 
-  res.status(201).json(user); 
+  const favouriteList = await user.populate('favourites')
+  let flag = favouriteList.favourites.filter((e) => e.id == newFavourite)
+  if(flag.length == 0) {
+    await user.favourites.push(movie._id);
+    await user.save(); 
+    res.status(201).json(user); 
+  }
+  else {
+    res.status(402).json({ code: 402, msg: 'Movie has been added' });
+  }
 }));
 
 
